c129a9ce8eaa97defe0dd5b8b9de2abf9e9f45c3
Pratham Arora <prathamarora9f15@gmail.com>
2026-09-07 15:22:02 +0300

    Modify routes for API base paths

---
+++ routes/tourRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const {
+  getAllTours,
+  getTourById,
+  createTour,
+  updateTour,
+  deleteTour,
+} = require("../controllers/tourControllers.js");
+
+const router = express.Router();
+
+router.get("/", getAllTours);
+router.post("/", createTour);
+router.get("/:tourId", getTourById);
+router.put("/:tourId", updateTour);
+router.delete("/:tourId", deleteTour);
+
+module.exports = router;
