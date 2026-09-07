da7b4c21f51b9953756dd1e53048217172b080c8
ridhi <ridhiodedara64@gmail.com>
2026-09-07 15:30:39 +0300

    Implement morgan middleware for logging

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const morgan = require("morgan"); // Step 1: Morgan require kiya
 
 const tourRouter = require("./routes/tourRouter.js");
 const userRouter = require("./routes/userRouter");
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
+app.use(morgan("tiny")); // Step 2: Morgan middleware attach kiya
 
+// Routes
 app.use("/api/tours", tourRouter);
 app.use("/api/users", userRouter);
 
